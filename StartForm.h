dca9ab20fd47100e9915e08dde5f7eb05b27a638
Jan Maliborski <jan.maliborski@gmail.com>
2025-09-04 11:38:51 +0200

    StartForm - stworzenie UI

--- StartForm.h
+++ StartForm.h
@@ -34,6 +34,17 @@ namespace PuzzleSolver {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ IntroGameLabel;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::ComboBox^ PlayerComboBox;
+	private: System::Windows::Forms::Label^ GameSizeLabel;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel1;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+
 
 	private:
 		/// <summary>
@@ -48,12 +59,143 @@ namespace PuzzleSolver {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"StartForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->IntroGameLabel = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->PlayerComboBox = (gcnew System::Windows::Forms::ComboBox());
+			this->GameSizeLabel = (gcnew System::Windows::Forms::Label());
+			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->flowLayoutPanel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// IntroGameLabel
+			// 
+			this->IntroGameLabel->AutoSize = true;
+			this->IntroGameLabel->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->IntroGameLabel->Location = System::Drawing::Point(173, 9);
+			this->IntroGameLabel->Name = L"IntroGameLabel";
+			this->IntroGameLabel->Size = System::Drawing::Size(418, 39);
+			this->IntroGameLabel->TabIndex = 0;
+			this->IntroGameLabel->Text = L"Uk³adanki z lat dziecinnych";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->label1->Location = System::Drawing::Point(15, 0);
+			this->label1->Margin = System::Windows::Forms::Padding(15, 0, 15, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(66, 26);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Gracz";
+			// 
+			// PlayerComboBox
+			// 
+			this->PlayerComboBox->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->PlayerComboBox->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->PlayerComboBox->FormattingEnabled = true;
+			this->PlayerComboBox->Location = System::Drawing::Point(15, 41);
+			this->PlayerComboBox->Margin = System::Windows::Forms::Padding(15);
+			this->PlayerComboBox->Name = L"PlayerComboBox";
+			this->PlayerComboBox->Size = System::Drawing::Size(305, 29);
+			this->PlayerComboBox->TabIndex = 3;
+			// 
+			// GameSizeLabel
+			// 
+			this->GameSizeLabel->AutoSize = true;
+			this->GameSizeLabel->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->GameSizeLabel->Location = System::Drawing::Point(15, 85);
+			this->GameSizeLabel->Margin = System::Windows::Forms::Padding(15, 0, 15, 0);
+			this->GameSizeLabel->Name = L"GameSizeLabel";
+			this->GameSizeLabel->Size = System::Drawing::Size(106, 26);
+			this->GameSizeLabel->TabIndex = 4;
+			this->GameSizeLabel->Text = L"GameSize";
+			// 
+			// flowLayoutPanel1
+			// 
+			this->flowLayoutPanel1->Controls->Add(this->label1);
+			this->flowLayoutPanel1->Controls->Add(this->PlayerComboBox);
+			this->flowLayoutPanel1->Controls->Add(this->GameSizeLabel);
+			this->flowLayoutPanel1->Controls->Add(this->numericUpDown1);
+			this->flowLayoutPanel1->Location = System::Drawing::Point(212, 132);
+			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
+			this->flowLayoutPanel1->Size = System::Drawing::Size(347, 181);
+			this->flowLayoutPanel1->TabIndex = 5;
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->numericUpDown1->Location = System::Drawing::Point(15, 126);
+			this->numericUpDown1->Margin = System::Windows::Forms::Padding(15);
+			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 3, 0, 0, 0 });
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->Size = System::Drawing::Size(305, 29);
+			this->numericUpDown1->TabIndex = 5;
+			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 3, 0, 0, 0 });
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(309, 413);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(141, 37);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Stwórz Gracza";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(309, 470);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(141, 37);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"Tabela wyników";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(309, 358);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(141, 37);
+			this->button3->TabIndex = 8;
+			this->button3->Text = L"Rozpocznij gre";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// StartForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::Desktop;
+			this->ClientSize = System::Drawing::Size(784, 561);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->flowLayoutPanel1);
+			this->Controls->Add(this->IntroGameLabel);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
+			this->Name = L"StartForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Uk³adanki z lat dziecinnych";
+			this->Load += gcnew System::EventHandler(this, &StartForm::StartForm_Load);
+			this->flowLayoutPanel1->ResumeLayout(false);
+			this->flowLayoutPanel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void StartForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
 	};
 }
